59a866ad5d3d5064a6b8396649da74d34fb0d05e
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2026-02-08 20:55:49 +0100

    updated user.py to consider Country, added user generation function

--- src/generators/generate_users.py
+++ src/generators/generate_users.py
@@ -1,17 +1,34 @@
 from faker import Faker
-fake = Faker()
+import random
+
+FAKERS = {
+    "DE": Faker("de_DE"),  # Germany
+    "FR": Faker("fr_FR"),  # France
+    "IT": Faker("it_IT"),  # Italy
+    "SE": Faker("sv_SE"),  # Sweden
+    "GB": Faker("en_GB"),  # United Kingdom
+}
 class User:
     @classmethod
     def generate(cls):
+        country_code = random.choice(list(FAKERS.keys()))
+        fake = FAKERS[country_code]
+
         first_name = fake.first_name()
         last_name = fake.last_name()
+
         return {
             "first_name": first_name,
             "last_name": last_name,
-            "email": f"{first_name.lower()}.{last_name.lower()}@example.com",
-            "address": fake.address(),
-            "phone_number": fake.phone_number()
+            "email": f"{first_name.lower()}.{last_name.lower()}@example.{country_code.lower()}",
+            "street": fake.street_address(),
+            "postal_code": fake.postcode(),
+            "city": fake.city(),
+            "country": fake.current_country(),
+            "country_code": country_code,
+            "phone_number": fake.phone_number(),
         }
+def genUsers(number):
+    for i in range(number):
+        yield User.generate()
 
-user_data = User.generate()
-print(user_data)
